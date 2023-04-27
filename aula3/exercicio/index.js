2ba4b0525d8dd549794615d4569154f3e1c1a92e
Rubens <rubenslemos@gmail.com>
2023-04-27 07:55:31 -0300

    exercicio aula 3

---
+++ aula3/exercicio/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Rubens". Estou aprendendo Javascript às ', 19, " horas" );
--- aula3/index.js
+++ aula3/index.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+console.log('hello world');     //String
+console.log("'hello' 'world'");
+console.log('"hello" "world"');
+console.log(`"hello" 'world'`);
+console.log(123345) //Number
+console.log(123345, 15.87, 'hello world');
