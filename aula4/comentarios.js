0ae6c22988a87427df9dab9f2e0102f059b01d32
Rubens <rubenslemos@gmail.com>
2023-04-27 09:57:43 -0300

    aula4

---
+++ aula4/comentarios.js
@@ -0,0 +1,8 @@
+//Escrever um comentário
+console.log ('Olá Mundo'); // outro comentário
+console.log ('Linhas não comentadas'); // outro comentário
+console.log ('Linhas não comentadas'); 
+console.log ('Linhas não comentadas'); 
+console.log ('Linhas não comentadas'); 
+/*Comentários com mais de uma linha devem usar esse formato
+pois caso o contrario usando o // ao mudar de linha a linha abaixo da que tem o // n sera um comentário */
