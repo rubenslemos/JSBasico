7146073ea95c783029700cef5f1798f55c0f96e1
Rubens <rubenslemos@gmail.com>
2023-04-28 16:07:56 -0300

    aula 11 refatorada

--- aula 11/js/index.js
+++ aula 11/js/index.js
@@ -5,8 +5,7 @@ let soma=confirm('Gostaria de realizar uma soma?');
 if (soma==true) {
   const numero1 = prompt('Digite um numero:');
   const numero2 = prompt('Digite um numero:');
-  const total = Number(numero1) + Number(numero2);
-  alert(`${numero1} + ${numero2} = ${total}`);
+  alert(`${numero1} + ${numero2} = ${Number(numero1) + Number(numero2)}`);
 }else{
   alert('obrigado por acessar nosso site');
 }
