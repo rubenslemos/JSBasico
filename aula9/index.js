b85ca744a7fb993fe9a5bb1768bcdcc6af863592
Rubens <rubenslemos@gmail.com>
2023-04-28 10:27:24 -0300

    aula9

---
+++ aula9/index.js
@@ -0,0 +1,19 @@
+const nome = 'Rubens'; //String
+const nome1 = "Rubens"; //String
+const nome2= `Rubens`; //String
+const num = 10; //number
+const num1 = 10.54; //number
+let variavelSemValorDeclarado; //undefined -> não aponta para nenhum local na memória por não possuir uma definição
+const nula = null //Nulo -> não aponta para nenhum local na memória por receber um valor nulo 
+const boleano = true; //boolean -> recebe valor verdade ou falso (lógico) 
+const boleano1 = false; //boolean -> recebe valor verdade ou falso (lógico)
+
+console.log(typeof nome, nome);
+console.log(typeof nome1, nome1);
+console.log(typeof nome2, nome2);
+console.log(typeof num, num);
+console.log(typeof num1, num1);
+console.log(typeof variavelSemValorDeclarado, variavelSemValorDeclarado);
+console.log(typeof nula, nula);
+console.log(typeof boleano, boleano);
+console.log(typeof boleano1, boleano1);
