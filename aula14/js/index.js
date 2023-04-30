bda5805725efcfbd2b810a7d03538bbe2774cef6
Rubens <rubenslemos@gmail.com>
2023-04-30 13:10:29 -0300

    aula14

---
+++ aula14/js/index.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite seu nome completo:');
+document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(1)} <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra e no seu nome? ${nome.indexOf('e')}<br />`;
+document.body.innerHTML += `Qual o último índice da letra e no seu nome? ${nome.lastIndexOf('e')}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(nome.length-3, nome.length)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
