8fbb01b042283b32ae3701aabe427e88874488d5
Rubens <rubenslemos@gmail.com>
2023-05-01 08:31:27 -0300

    correção de variavel pra constante na aula 18

--- aula18/index.js
+++ aula18/index.js
@@ -5,8 +5,8 @@ console.log(`Nomes: ${nomes}`);
 /* os elementos do array são indexados assim como os elementos de uma variável 
 qualquer, mas sua indexação é um pouco diferente pois o índice é para o elemento
 e não para os caracteres desse elemento */
-console.log(`Nomes: ${nomes}`[0]);
-console.log(`Nomes: ${nomes}`[1]);
+console.log(`Nomes: ${nomes[0]}`);
+console.log(`Nomes: ${nomes[1]}`);
 //podemos mudar o elemento do array
 nomes[1] = 'Lemos';
 console.log(`Nomes: ${nomes}`);
@@ -22,13 +22,13 @@ nomes.unshift('Francisco');
 nomes.unshift('José');
 console.log(`Nomes: ${nomes}`);
 //remover no final do array
-let removidos = [nomes.pop()];
+const removidos = [nomes.pop()];
 removidos.push(nomes.pop());
 removidos.push(nomes.pop());
 console.log(`Nomes: ${nomes}`);
 console.log(`Removidos do final do array: ${removidos}`);
 //remover no inicio do array
-let removido = [nomes.shift()];
+const removido = [nomes.shift()];
 removido.push(nomes.shift());
 removido.push(nomes.shift());
 console.log(`Nomes: ${nomes}`);
