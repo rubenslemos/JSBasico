de93f91cb882217a1fd7c8141dbcab697a096027
Rubens <rubenslemos@gmail.com>
2023-05-21 07:21:22 -0300

    aula66

--- Secao6-JS-ObjetosEprototypes(Avancado)/aula66/index.js
+++ Secao6-JS-ObjetosEprototypes(Avancado)/aula66/index.js
@@ -1,19 +1,45 @@
 /* 
-  
+  Além das definições ja vistas para Getters e Setters eles servem para proteger 
+  uma propriedade usando os Getters para obter valores e Setters para atribui-los
+  com isso dentro de Object.defineProperty e Object.defineProperties deixamos de
+  usar as configurações de writable e value, substituindo-as pelos métodos get e
+  set respectivamente.
 */
 function Produto (nome, preco, estoque) {
   this.nome = nome;
   this.preco = preco;
+  let estoquePrivado = estoque;
   //Congelando uma chave e uma ou atribuição da mesma
   Object.defineProperty(this, "estoque",{
     enumerable: true, //exibível ou não
-    value: estoque, //possui valor ou não
-    writable: false, //modificável ou não
-    configurable: true //reconfigurável ou não
+    configurable: true, //reconfigurável ou não
+    get: function () {
+      return estoquePrivado;
+    },
+    set: function (valor) {
+      if (typeof valor !== 'number')
+        throw new TypeError('O estoque deve ser um número');
+      estoquePrivado = valor;
+    }
   })
 }
-/* 
- 
-*/
 const produto = new Produto ('Camiseta', 20, 3);
 console.log(produto)
+produto.estoque = 156132;
+console.log(produto.estoque)
+
+//Usando Getter e Setter na factory function
+const criaProduto = (nome) =>{
+  return {
+    get nome (){
+      return nome;
+    },
+    set nome(valor) {
+      valor = valor.replace('Camisa', 'Bermuda');
+      nome=valor;
+    }
+  };
+}
+const novoProduto = criaProduto('Short');
+novoProduto.nome = 'Camisa';
+console.log(novoProduto.nome)
