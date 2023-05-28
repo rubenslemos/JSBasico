40f273fda58f876d95ef5cef04e378398ee8e112
Rubens <rubenslemos@gmail.com>
2023-05-28 10:25:19 -0300

    refatorado

--- Secao6-JS-Obj-Protos/aula72-3/assets/js/index.js
+++ Secao6-JS-Obj-Protos/aula72-3/assets/js/index.js
@@ -8,11 +8,11 @@ function ValidaDocumento (doc){
       return doc.replace(/\D+/g, '');
     }
   })
-}
+};
+
 ValidaDocumento.prototype.valida = function(){
-  console.log(this.documentoLimpo);
-  if (typeof this.documentoLimpo === 'undefined') return false;
-  if(this.documentoLimpo.length !== 11 && this.documentoLimpo.length !== 14) return false;
+  typeof this.documentoLimpo === 'undefined' ? false : (this.documentoLimpo.length !== 11 && this.documentoLimpo.length !== 14 ? false : true);
+
   if (this.isSequencia()) return false;
   const documentoParcial = this.documentoLimpo.slice(0,-2);
   const digito1 = this.criaDigito(documentoParcial);
@@ -24,15 +24,15 @@ ValidaDocumento.prototype.valida = function(){
 ValidaDocumento.prototype.criaDigito = function(documentoParcial){
   const documentoArray = Array.from(documentoParcial);
   if(documentoArray.length >= 12 ){
-  let regressivo = documentoArray.length -7;
-  const total = documentoArray.reduce((acumulador, numDocumento) => {
-    acumulador += (regressivo * Number(numDocumento));
-    --regressivo;
-    regressivo === 1 ? regressivo = 9 : regressivo
-    return acumulador;
-  },0);
-  const digito = (total % 11);
-  return digito < 2 ? '0' : String(11-digito)
+    let regressivo = documentoArray.length -7;
+    const total = documentoArray.reduce((acumulador, numDocumento) => {
+      acumulador += (regressivo * Number(numDocumento));
+      --regressivo;
+      regressivo === 1 ? regressivo = 9 : regressivo
+      return acumulador;
+    },0);
+    const digito = (total % 11);
+    return digito < 2 ? '0' : String(11-digito)
   }else {
     regressivo = documentoArray.length +1;
     const total = documentoArray.reduce((acumulador, numDocumento) => {
@@ -90,15 +90,7 @@ const criarBotaoApagar = (li) => {
 btnVerifica.addEventListener('click', () => {
   if (!inputVerifica.value) return;
   const documento = new ValidaDocumento(inputVerifica.value);
-  if (documento.valida()){
-    if(documento.documentoLimpo.length === 14){
-      inputVerifica.value +=' - CNPJ Valido';
-    }else{
-      inputVerifica.value += ' - CPF Válido';
-    }
-  }else{
-    inputVerifica.value += ' - Documento Inválido';
-  }  
+  inputVerifica.value += documento.valida() ? ((documento.documentoLimpo.length === 14) ? ' - CNPJ Válido' : ' - CPF Válido') : ' - Documento Inválido';
   criaTarefa(inputVerifica.value);
   limpaInput()
 });
@@ -107,19 +99,11 @@ inputVerifica.addEventListener('keypress', (event) => {
   if (event.key === 'Enter'){ 
     if (!inputVerifica.value) return;
     const documento = new ValidaDocumento(inputVerifica.value);
-    if (documento.valida()){
-      if(documento.documentoLimpo.length === 14){
-        inputVerifica.value +=' - CNPJ Valido';
-      }else{
-        inputVerifica.value += ' - CPF Válido';
-      }
-    }else{
-      inputVerifica.value += ' - Documento Inválido';
-    }  
-    criaTarefa(inputVerifica.value);
-    limpaInput()
-  }
-});
+    inputVerifica.value += documento.valida() ? ((documento.documentoLimpo.length === 14) ? ' - CNPJ Válido' : ' - CPF Válido') : ' - Documento Inválido';
+
+  criaTarefa(inputVerifica.value);
+  limpaInput();
+}});
 
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-apagar')){
