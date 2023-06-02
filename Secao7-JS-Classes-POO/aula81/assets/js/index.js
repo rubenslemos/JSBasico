f3332ee4b0749dbd48cf102e38efd8b9a20cf75c
Rubens <rubenslemos@gmail.com>
2023-06-02 07:25:33 -0300

    fim aula81

--- Secao7-JS-Classes-POO/aula81/assets/js/index.js
+++ Secao7-JS-Classes-POO/aula81/assets/js/index.js
@@ -12,6 +12,11 @@ class ValidaFormulario {
   handleSubmit(e){
     e.preventDefault();
     const camposValidos = this.camposValidos();
+    const senhaValida = this.senhaValida();
+    if (camposValidos && senhaValida) {
+      alert('Formulário enviado.');
+  /*     this.formulario.submit(); */
+    }
   }
   camposValidos(){
     let validos = true;
@@ -30,11 +35,42 @@ class ValidaFormulario {
       if(campo.classList.contains('usuario')){
         if(!this.validaUsuario(campo)) validos = false;
       }   
-
     }
+    return validos;
   }
-  validaUsuario(){
+
+  senhaValida(){
+    let validos = true;
+
+    const senha = this.formulario.querySelector('.senha');
+    const repetirSenha = this.formulario.querySelector('.repetir-senha')
+    console.log(senha, repetirSenha);
+    if(senha.value !== repetirSenha.value){
+      this.criaErro(senha, 'Senhas diferentes entre si');
+      this.criaErro(repetirSenha, 'Senhas diferentes entre si');
+      validos = false;
+    }
+    if(senha.value.length <6 || senha.value.length >12) {
+      this.criaErro(senha, 'A senha deverá ter tamanho de 6 a 12 caracteres');
+      this.criaErro(repetirSenha, 'A senha deverá ter tamanho de 6 a 12 caracteres');
+      validos = false;
+    }    
+    return validos;
+  }
+
+  validaUsuario(campo){
+    const usuario = campo.value;
+    let user = true;
+    if(usuario.length <3 || usuario.length >12) {
+      this.criaErro(campo, 'Usuário deverá ter tamanho de 3 a 12 caracteres');
+      user = false;
+    }    
+    if(!usuario.match(/^[A-Za-z0-9]+$/g)) {
+      this.criaErro(campo, 'Usuário só poderá conter letras e/ou números');
+      user = false;
+    }
     
+    return user;
   }
   validaCPF(campo) {
     const cpf = new ValidaDocumento(campo.value);
